3a8dde94210d0fe17ecc37480ce5353bc8ac2cce
= <tungha2206@gmail.com>
2025-05-10 23:44:44 +0700

    add ui sign in

---
+++ app/src/main/java/com/example/newsapp/presentation/ui/component/home/SignInFragment.kt
@@ -0,0 +1,22 @@
+package com.example.newsapp.presentation.ui.component.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.newsapp.R
+import com.example.newsapp.databinding.FragmentSignInBinding
+import com.example.newsapp.presentation.base.BaseFragment
+
+
+class SignInFragment : BaseFragment<FragmentSignInBinding>(FragmentSignInBinding::inflate) {
+
+    override fun initListener() {
+
+    }
+
+    override fun initUi() {
+
+    }
+}
