ad7764c1cc7815e56f286338d1dd7f14d0e82e48
= <tungha2206@gmail.com>
2025-05-11 00:50:19 +0700

    set up bottom nav & navigation

--- app/src/main/java/com/example/newsapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/presentation/ui/MainActivity.kt
@@ -1,21 +1,60 @@
 package com.example.newsapp.presentation.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.newsapp.R
+import com.example.newsapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setUpBottomNav()
     }
+
+    fun setUpBottomNav() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavView.apply {
+            setupWithNavController(navController)
+        }
+
+        navController.addOnDestinationChangedListener(object :
+            NavController.OnDestinationChangedListener {
+            override fun onDestinationChanged(
+                controller: NavController,
+                destination: NavDestination,
+                arguments: Bundle?
+            ) {
+                when (destination.id) {
+                    R.id.splashFragment, R.id.signInFragment -> {
+                        binding.bottomNavView.visibility = View.GONE
+                    }
+                    else -> binding.bottomNavView.visibility = View.VISIBLE
+                }
+
+            }
+        })
+
+
+    }
+
 }
