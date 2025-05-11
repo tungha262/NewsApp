ac4ab23a9a30db12c6cd9b087be7268cf9377a89
= <tungha2206@gmail.com>
2025-05-11 08:09:28 +0700

    add ui sign up

---
+++ app/src/main/java/com/example/newsapp/presentation/ui/component/auth/SignUpFragment.kt
@@ -0,0 +1,20 @@
+package com.example.newsapp.presentation.ui.component.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.newsapp.R
+import com.example.newsapp.databinding.FragmentSignUpBinding
+import com.example.newsapp.presentation.base.BaseFragment
+
+class SignUpFragment : BaseFragment<FragmentSignUpBinding>(FragmentSignUpBinding::inflate) {
+    override fun initListener() {
+
+    }
+
+    override fun initUi() {
+
+    }
+}
