5ddc92158c59bea22c9fcf5881886e456bb68667
= <tungha2206@gmail.com>
2025-05-16 22:55:02 +0700

    add local viewModel

--- app/src/main/java/com/example/newsapp/presentation/viewModel/LocalViewModel.kt
+++ app/src/main/java/com/example/newsapp/presentation/viewModel/LocalViewModel.kt
@@ -1,13 +1,71 @@
 package com.example.newsapp.presentation.viewModel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.newsapp.data.model.Article
 import com.example.newsapp.domain.repo.LocalRepository
+import com.example.newsapp.domain.state.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LocalViewModel @Inject constructor(
     private val localRepository: LocalRepository
 ) : ViewModel() {
+
+    private var _addFavoriteArticle= MutableSharedFlow<Resource<String>>()
+    val addFavoriteArticle : SharedFlow<Resource<String>> get() = _addFavoriteArticle
+
+    private var _getAllFavoriteArticle = MutableLiveData<Resource<List<Article>>>()
+    val getAllFavoriteArticle : LiveData<Resource<List<Article>>> get() = _getAllFavoriteArticle
+
+    private var _deleteFavoriteArticle = MutableSharedFlow<Resource<String>>()
+    val deleteFavoriteArticle : SharedFlow<Resource<String>> get() = _deleteFavoriteArticle
+
+    private var _deleteAllFavoriteArticle = MutableSharedFlow<Resource<String>>()
+    val deleteAllFavoriteArticle : SharedFlow<Resource<String>> get() = _deleteAllFavoriteArticle
+
+
+    fun addFavoriteArticle(article: Article){
+        viewModelScope.launch(Dispatchers.IO) {
+            _addFavoriteArticle.emit(localRepository.addFavoriteArticle(article))
+        }
+    }
+
+    fun getAllFavoriteArticle(){
+        viewModelScope.launch {
+            localRepository.getAllFavoriteArticle().collect { rs ->
+                when(rs){
+                    is Resource.Loading -> _getAllFavoriteArticle.value = Resource.Loading
+                    is Resource.Success -> {
+                        val data = rs.data
+                        _getAllFavoriteArticle.value = Resource.Success(data)
+                    }
+                    is Resource.Failed -> {
+                        _getAllFavoriteArticle.value = Resource.Failed(rs.message)
+                    }
+                }
+            }
+        }
+    }
+
+    fun deleteFavoriteArticle(article: Article){
+        viewModelScope.launch(Dispatchers.IO) {
+            _deleteFavoriteArticle.emit(localRepository.deleteFavoriteArticle(article))
+        }
+    }
+
+    fun deleteAllFavoriteArticle(){
+        viewModelScope.launch(Dispatchers.IO) {
+            _deleteAllFavoriteArticle.emit(localRepository.deleteAllFavoriteArticle())
+        }
+    }
+
 
 }
